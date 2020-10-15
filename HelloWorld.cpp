0993039571a45689af2fd140ad49cd64ff20fa8b
Sonchaegeon <64083083+Sonchaegeon@users.noreply.github.com>
2020-10-15 09:33:26 +0900

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	//std는 'Namespace'라고 하며 개념상 '소속'이라고 생각
+	//::은 '범위 지정 연산자' 혹은 '스코프 설정 연산자(Scope resolution operator)'라고 함
+	//cout은 콘솔 출력을 담당하는 객체
+	//<<은 '연산자 함수'
+	std::cout << "Hello world! \n";
+
+	return 0;
+}
