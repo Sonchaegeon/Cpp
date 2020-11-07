caa02d7d3d2c3baaa165e3c4f592b57cf1b31639
Sonchaegeon <thscorjs1002@naver.com>
2020-11-07 21:09:20 +0900

    Update StringCtrlSample1111.cpp

--- Homework/StringCtrlSample1111.cpp
+++ Homework/StringCtrlSample1111.cpp
@@ -5,12 +5,14 @@ using namespace	std;
 
 int main() {
 	CMyStringEx strTest;
-	strTest.SetString("");
+
+	// 문자열이 필터링 되어 대체되는 경우
+	strTest.SetString("장애인");
 	cout << strTest << endl;
 
-	int nIndex = strTest.Find("손채건");
-
-	cout << "Index : " << nIndex << endl;
-
+	// 필터링 되지 않은 경우
+	strTest.SetString("학번 : 1111, 이름 : 손채건");
+	cout << strTest << endl;
+	
 	return 0;
 }
