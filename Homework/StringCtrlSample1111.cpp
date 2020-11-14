5d0899a3920618b6a46ec5c628a60feb6d180003
Sonchaegeon <thscorjs1002@naver.com>
2020-11-14 19:32:09 +0900

    12

--- Homework/StringCtrlSample1111.cpp
+++ Homework/StringCtrlSample1111.cpp
@@ -4,9 +4,15 @@
 using namespace	std;
 
 int main() {
-	CMyStringEx strLeft("학번:1111"), strRight(" 이름:손채건");
+	CMyStringEx strTest;
 
-	cout << strLeft << strRight << endl;
+	// 문자열이 필터링 되어 대체되는 경우
+	strTest.SetString("멍멍이아들");
+	cout << strTest << endl;
+
+	// 필터링 되지 않은 경우
+	strTest.SetString("학번 : 1234, 이름 : 홍길동");
+	cout << strTest << endl;
 
 	return 0;
 }
