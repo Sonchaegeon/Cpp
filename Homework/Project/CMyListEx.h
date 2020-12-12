1cea35ab6e358eb436e22a2bb5b43ba6debead31
Sonchaegeon <thscorjs1002@naver.com>
2020-12-13 00:14:27 +0900

    InsertFront, InsertBack, DeleteFront, Print

---
+++ Homework/Project/CMyListEx.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "CMyList.h"
+class CMyListEx :
+    public CMyList
+{
+public:
+    int InsertFront();
+    void Print();
+private:
+    DNode* headNode;
+    DNode* tailNode;
+    int m_nLength;
+public:
+    void InsertBack();
+    void DeleteFront();
+    void DeleteBack();
+};
+
