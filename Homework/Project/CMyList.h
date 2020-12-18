6057a4c82a8aa80921ea9f1efb1a0d8fee700524
Sonchaegeon <thscorjs1002@naver.com>
2020-12-18 19:30:41 +0900

    finish

--- Homework/Project/CMyList.h
+++ Homework/Project/CMyList.h
@@ -4,17 +4,22 @@ class CMyList
 public:
 	typedef struct DNode {
 		int data;
+		DNode* prev;
 		DNode* link;
 	} DNode;
 	CMyList();
-	void ShowMenu();
-	void Insert();
 
+	virtual void ShowMenu();
+	void Insert();
 private:
 	DNode* headNode;
 	DNode* tailNode;
 	int m_nLength;
 public:
 	void Print();
+	DNode* GetHeadNode() const;
+	DNode* GetTailNode() const;
+	void SetHeadNode(DNode* link) { headNode = link; }
+	void SetTailNode(DNode* link) { tailNode = link; }
 };
 
