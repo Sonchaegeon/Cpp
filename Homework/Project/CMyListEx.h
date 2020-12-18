e4e69f1db64075c7937707988f8b8c914fdaa36e
Sonchaegeon <thscorjs1002@naver.com>
2020-12-18 19:31:28 +0900

    finish

--- Homework/Project/CMyListEx.h
+++ Homework/Project/CMyListEx.h
@@ -1,19 +1,31 @@
 #pragma once
 #include "CMyList.h"
-class CMyListEx :
-    public CMyList
+class CMyListEx : public CMyList
 {
 public:
+    CMyListEx() {
+        headNode = nullptr;
+        tailNode = nullptr;
+        m_nLength = 0;
+    }
+    CMyListEx(CMyList* list) {
+        headNode = list->GetHeadNode();
+        tailNode = list->GetTailNode();
+        m_nLength = 0;
+    }
+
+    static int printCount;
+
     int InsertFront();
     void Print();
-private:
-    DNode* headNode;
-    DNode* tailNode;
-    int m_nLength;
-public:
     void InsertBack();
     void DeleteFront();
     void DeleteBack();
     void PrintReverse();
+    void operator--();
+private:
+    DNode* headNode;
+    DNode* tailNode;
+    int m_nLength;
 };
 
