c12c863e7f26c6323bcf80e3d5516d98837abac5
Sonchaegeon <thscorjs1002@naver.com>
2020-12-18 19:30:26 +0900

    finish

--- Homework/Project/CMyList.cpp
+++ Homework/Project/CMyList.cpp
@@ -11,11 +11,16 @@ CMyList::CMyList() {
 void CMyList::ShowMenu()
 {
 	cout << "--------------------" << endl;
-	cout << "CMyList 메뉴" << endl;
-	cout << "1 삽입" << endl;
-	cout << "2 출력" << endl;
+	cout << "CMyListEx 메뉴" << endl;
+	cout << "1 삽입 Head" << endl;
+	cout << "2 삽입 Tail" << endl;
+	cout << "3 삭제 Head" << endl;
+	cout << "4 삭제 Tail" << endl;
+	cout << "5 출력 Head -> Tail" << endl;
+	cout << "6 출력 Tail -> Head" << endl;
+	cout << "7 CMyListEx--" << endl;
 	cout << "0 종료" << endl;
-	cout << "--------------------" << endl;
+	cout << "--------------------\n" << endl;
 	cout << "메뉴를 고르세요(숫자 입력) : ";
 }
 
@@ -29,9 +34,10 @@ void CMyList::Insert()
 	if (headNode == nullptr) {
 		headNode = node;
 		tailNode = node;
+		node->prev = nullptr;
 	}
 	else {
-		tailNode->link = node;
+		node->prev = tailNode;
 		tailNode = node;
 	}
 	Print();
@@ -41,14 +47,25 @@ void CMyList::Insert()
 void CMyList::Print()
 {
 	DNode* temp = headNode;
-	int count = 0;
+
 	cout << "[Head] ";
 	while (temp != nullptr) {
 		cout << temp->data;
 		if (temp->link != nullptr) cout << " - ";
-		count++;
+		m_nLength++;
 		temp = temp->link;
 	}
 	cout << " [Tail]\n";
-	cout << "데이터 개수 : " << count << "개\n\n";
+	cout << "데이터 개수 : " << m_nLength << "개\n\n";
+}
+
+
+CMyList::DNode* CMyList::GetHeadNode() const
+{
+	return headNode;
+}
+
+CMyList::DNode* CMyList::GetTailNode() const
+{
+	return tailNode;
 }
