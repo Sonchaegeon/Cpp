841cf4d17931a7d1d7a21857d4f07654af7d2cd7
Sonchaegeon <thscorjs1002@naver.com>
2020-12-18 19:31:46 +0900

    finish

--- Homework/Project/Main.cpp
+++ Homework/Project/Main.cpp
@@ -3,7 +3,10 @@
 using namespace	std;
 int	main()
 {
-	CMyListEx* myListex = new CMyListEx;
+	CMyList* myList = new CMyList();
+	myList->Insert();
+	CMyListEx* myListex = new CMyListEx(myList);
+
 	int	num;
 	while (1)
 	{
@@ -13,7 +16,7 @@ int	main()
 		{
 			cin.clear();
 			cin.ignore(10000, '\n');
-			cout << "0,	1, 2, 3, 4, 5, 6 중 숫자를 입력하세요~! :	";
+			cout << "0,	1, 2, 3, 4, 5, 6, 7 중 숫자를 입력하세요~!" << endl;;
 			continue;
 		}
 		switch (num)
@@ -22,7 +25,6 @@ int	main()
 			break;
 		case 2: myListex->InsertBack();
 			break;
-			
 		case 3: myListex->DeleteFront();
 			break;
 		case 4: myListex->DeleteBack();
@@ -31,11 +33,14 @@ int	main()
 			break;
 		case 6: myListex->PrintReverse();
 			break;
-		case 0: cout << "	종료합니다!\n";
+		case 7: 
+			myListex->operator--();
+			break;
+		case 0: cout << "종료합니다!\n";
 			delete	myListex;
 			return	0;
 		default:
-			cout << "0, 1, 2, 3, 4, 5, 6 중 숫자를 입력하세요~! : ";
+			cout << "0, 1, 2, 3, 4, 5, 6, 7 중 숫자를 입력하세요~! : " << endl;;
 			continue;
 		}
 	}
